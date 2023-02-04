3152d66a1599aee2014b8ee07c71f54d03dc233f
fidan2004 <110298605+fidan2004@users.noreply.github.com>
2023-02-04 23:35:59 +0400

    Add files via upload

---
+++ WebApplication2/WebApplication2/Controllers/Animal.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication2.Controllers
+{
+    public class Animal:IAnimal
+    {
+        public string GetAnimal()
+        {
+            return "Aslan";
+        }
+    }
+    public interface IAnimal
+    {
+        string GetAnimal();
+    }
+}
---
+++ WebApplication2/WebApplication2/Controllers/AnimalController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+
+        private readonly IAnimal _animal;
+
+        public AnimalController(IAnimal animal)
+        {
+            _animal = animal;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _animal.GetAnimal();
+        }
+    }
+}
